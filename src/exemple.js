6bee74a7d99243589cde45889874602e2f73b909
otavioschein <otaviomschein@gmail.com>
2020-06-13 14:30:34 -0300

    first files inserted

---
+++ src/exemple.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <div>
+      <p>Você clicou {count} vezes</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clique aqui
+      </button>
+    </div>
+  );
+}
