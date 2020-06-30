030e81f825264e00020d24469cdc7de991bd1e0c
otavioschein <otaviomschein@gmail.com>
2020-06-30 19:49:05 -0300

    finished sjf and round-robin with chart in the terminal

--- roundRobin.py
+++ roundRobin.py
@@ -1,9 +1,16 @@
+class objArray:
+    def __init__(self, name, startTime, fullTimeProcess, array):
+        self.name = name
+        self.startTime = startTime
+        self.fullTimeProcess = fullTimeProcess
+        self.array = array
+
 def bubbleSortByStartTime(dataArray):
     n = len(dataArray)
     for i in range(n-1):
-        for j in range(0, n-i-1):
-            if dataArray[j].startTime > dataArray[j+1].startTime:
-                dataArray[j], dataArray[j+1] = dataArray[j+1], dataArray[j]
+        for j in range(0, n - i - 1):
+            if dataArray[j].startTime > dataArray[j + 1].startTime:
+                dataArray[j], dataArray[j + 1] = dataArray[j + 1], dataArray[j]
     return dataArray
 
 def appendProcess(queue, count, aptQueue):
@@ -13,54 +20,76 @@ def appendProcess(queue, count, aptQueue):
                 aptQueue.append(i)
     return aptQueue    
 
-def executionRoundRobin(dataArray, quantum, executingProcess):
-    aptQueue = []
+def printChart(queue):
+    for i in queue:
+        print('Process ', i.name, end=': ')
+        for j in i.array:
+            print(j, end="")
+        print()
+
+def executionRoundRobin(dataArray, quantum, executingProcess): # where the magic happens
+    readyArray = [] # array of completed processes
+    aptQueue = [] 
     count = 0
-    done = False
+    done = False # represents a completed process
     timeExecution = 0
     totalFullTimeProcesses = dataArray[0].startTime
-    for i in dataArray:
+    for x in dataArray: # to get empty spaces in the chart, at the beggining of the process
+        for y in range(x.startTime):
+            x.array.append(" ")
+    for i in dataArray: # get a number of the sum of all processes's duration
         totalFullTimeProcesses += i.fullTimeProcess
     print(totalFullTimeProcesses)
-    while count < totalFullTimeProcesses:
+    while count < totalFullTimeProcesses: 
         done = False
         aptQueue = appendProcess(dataArray, count, aptQueue)
-        timeExecution = count + quantum
-        executingProcess = aptQueue[0]
-        del aptQueue[0]
-        while count < timeExecution and not done:
-            print('Process executing: ', executingProcess.name)
+        try:
+            timeExecution = count + quantum # simulate a quantum
+            executingProcess = aptQueue[0]
+            del aptQueue[0]
+            while count < timeExecution and not done:
+                executingProcess.array.append("=") # symbol that a process is executing
+                for l in aptQueue:
+                    l.array.append("-") # symbol that a process is apt but not executing
+                count += 1
+                executingProcess.fullTimeProcess -= 1
+                aptQueue = appendProcess(dataArray, count, aptQueue)
+                if executingProcess.fullTimeProcess == 0:
+                    done = True
+                    readyArray.append(executingProcess)
+            if not done:
+                aptQueue.append(executingProcess)
+        except:
             count += 1
-            executingProcess.fullTimeProcess -= 1
-            aptQueue = appendProcess(dataArray, count, aptQueue)
-            if executingProcess.fullTimeProcess == 0:
-                done = True
-        if not done:
-            aptQueue.append(executingProcess)
-            
+    printChart(readyArray)      
 
-class objArray:
-    def __init__(self, id, name, startTime, fullTimeProcess):
-        self.id = id
-        self.name = name
-        self.startTime = startTime
-        self.fullTimeProcess = fullTimeProcess
+def getInput(amount): # get the main inputs, name, start time, full time and put in an array
+    processArray = []
+    sumOfTime = 0
+    for i in range(amount):
+        name = str(input('Name of the process: '))
+        startTime = int(input('Time of insertion: '))
+        fullTimeProcess = int(input('Duration of the process: '))
+        array = []
+        sumOfTime += fullTimeProcess
+        if sumOfTime > 80:
+            print("You've reached the maximum time of duration! The last process will be killed.") 
+            return processArray
+        process = objArray(name, startTime, fullTimeProcess, array)
+        processArray.append(process)
+        print()
+    return processArray
 
-dataArray = []
+def main(): # Does a loop to get the right number of processes, calls the functions to get the inputs, sort the main array and finally, executes the algorithm.
+    amountProcesses = 11
+    executingProcess = objArray('', 0, 0, [])
+    print('Welcome to the Round-Robin!')
+    while amountProcesses > 10:
+        amountProcesses = int(input('How many processes will it be? 10 processes is the maximum: '))
+    quantum = int(input('Inform the quantum: '))
+    dataArray = getInput(amountProcesses)
+    newArray = bubbleSortByStartTime(dataArray)
+    executionRoundRobin(newArray, quantum, executingProcess)
 
-dataArray.append(objArray(1, 'A', 0, 4))
-dataArray.append(objArray(3, 'B', 5, 2))
-dataArray.append(objArray(2, 'C', 2, 3))
-dataArray.append(objArray(4, 'D', 0, 5))
-
-executingProcess = objArray(0, '', 0, 0)
-
-#dataArray.append(objArray(1, 'A', 2, 6))
-#dataArray.append(objArray(3, 'B', 0, 5))
-#dataArray.append(objArray(2, 'C', 1, 2))
-#dataArray.append(objArray(4, 'D', 3, 4))
-
-quantum = 3
-
-newArray = bubbleSortByStartTime(dataArray)
-executionRoundRobin(newArray, quantum, executingProcess)
+if __name__ == "__main__":
+    main()
--- sjf.py
+++ sjf.py
@@ -1,3 +1,10 @@
+class objArray:
+    def __init__(self, name, startTime, fullTimeProcess, array):
+        self.name = name
+        self.startTime = startTime
+        self.fullTimeProcess = fullTimeProcess
+        self.array = array
+
 def bubbleSortByStartTime(dataArray):
     n = len(dataArray)
     for i in range(n-1):
@@ -14,48 +21,77 @@ def bubbleSortByFullTimeProcess(array):
                 array[j], array[j+1] = array[j+1], array[j]
     return array
 
-def executionSJF(dataArray):
-    readyQueue = []
+def appendProcess(queue, count, aptQueue):
+    for i in queue:
+        if count == i.startTime:
+            if i not in (aptQueue):
+                aptQueue.append(i)
+    return aptQueue
+
+def printChart(queue):
+    for i in queue:
+        print('Process ', i.name, end=': ')
+        for j in i.array:
+            print(j, end="")
+        print()
+
+def executionSJF(dataArray, executingProcess): # where the magic happens
+    readyArray = [] # array of completed processes
+    aptQueue = []
     totalFullTimeProcesses = dataArray[0].startTime
     count = 0
     timeExecution = 0
+    for x in dataArray: # to get empty spaces in the chart, at the beggining of the process
+        for y in range(x.startTime):
+            x.array.append(" ")
     for k in dataArray:
         totalFullTimeProcesses += k.fullTimeProcess
     print(totalFullTimeProcesses)    
     while count < totalFullTimeProcesses:
-        for i in dataArray:
-            if count == i.startTime:
-                readyQueue.append(i)
-        readyQueue = bubbleSortByFullTimeProcess(readyQueue)        
-        timeExecution = readyQueue[0].fullTimeProcess + count
-        while count < timeExecution:
-            print('Process executing: ', readyQueue[0].name)
+        aptQueue = appendProcess(dataArray, count, aptQueue)
+        aptQueue = bubbleSortByFullTimeProcess(aptQueue) 
+        try:
+            timeExecution = aptQueue[0].fullTimeProcess + count
+            executingProcess = aptQueue[0] 
+            del aptQueue[0] # that process is not needed anymore in this queue
+            while count < timeExecution:
+                executingProcess.array.append("=") # symbol that a process is executing
+                for l in aptQueue:
+                    l.array.append("-") # symbol that a process is apt but not executing
+                count += 1
+                aptQueue = appendProcess(dataArray, count, aptQueue)
+            readyArray.append(executingProcess)
+            aptQueue = bubbleSortByFullTimeProcess(aptQueue)
+        except:
             count += 1
-            for j in dataArray:
-                if count == j.startTime:
-                    readyQueue.append(j)
-        del readyQueue[0]
-        readyQueue = bubbleSortByFullTimeProcess(readyQueue)
+    printChart(readyArray)
 
+def getInput(amount): # get the main inputs, name, start time, full time and put in an array
+    processArray = []
+    sumOfTime = 0
+    for i in range(amount):
+        name = str(input('Name of the process: '))
+        startTime = int(input('Time of insertion: '))
+        fullTimeProcess = int(input('Duration of the process: '))
+        sumOfTime += fullTimeProcess
+        if sumOfTime > 80:
+            print("You've reached the maximum time of duration! The last process will be killed.")
+            return processArray
+        array = []
+        process = objArray(name, startTime, fullTimeProcess, array)
+        processArray.append(process)
+        print()
+    return processArray
 
-class objArray:
-    def __init__(self, id, name, startTime, fullTimeProcess):
-        self.id = id
-        self.name = name
-        self.startTime = startTime
-        self.fullTimeProcess = fullTimeProcess
+def main(): # Does a loop to get the right number of processes, calls the functions to get the inputs, sort the main array and finally, executes the algorithm.
+    amountProcesses = 11
+    executingProcess = objArray('', 0, 0, [])
+    print('Welcome to the SJF non preemptive!')
+    while amountProcesses > 10:
+        amountProcesses = int(input('How many processes will it be? 10 processes is the maximum: '))
+    dataArray = getInput(amountProcesses)
+    newArray = bubbleSortByStartTime(dataArray)
+    executionSJF(newArray, executingProcess)
 
-dataArray = []
-
-#dataArray.append(objArray(1, 'A', 0, 4))
-#dataArray.append(objArray(3, 'B', 5, 2))
-#dataArray.append(objArray(2, 'C', 2, 3))
-#dataArray.append(objArray(4, 'D', 0, 5))
-
-dataArray.append(objArray(1, 'A', 2, 6))
-dataArray.append(objArray(3, 'B', 0, 5))
-dataArray.append(objArray(2, 'C', 1, 2))
-dataArray.append(objArray(4, 'D', 3, 4))
-
-newArray = bubbleSortByStartTime(dataArray)
-executionSJF(newArray)
+if __name__ == "__main__":
+    main()
