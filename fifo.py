96f29a9634bddea7ca7a30e0b910371cb494c372
augusto-marques <marques@DESKTOP-98KS9KU.localdomain>
2020-07-01 17:17:17 -0300

    Add time constraint to fifo

--- fifo.py
+++ fifo.py
@@ -1,10 +1,9 @@
 class Process:
     __lastId = 1
 
-    def __init__(self, duration, insertTime, priority):
+    def __init__(self, duration, insertTime):
         self.duration = duration
         self.insertTime = insertTime
-        self.priority = priority
         self.id = Process.__lastId
         self.chart = ""
         Process.__lastId += 1
@@ -21,6 +20,7 @@ def order(procs):
 
 def ganttFifo(procs):
     waitTime = procs[0].insertTime
+    print("")
     for proc in procs:
         print("Process ", proc.id, end=": ")
         for i in range(proc.insertTime):
@@ -28,24 +28,32 @@ def ganttFifo(procs):
         for i in range(waitTime - proc.insertTime):
             print("-", end="")
         for i in range(proc.duration):
-            print("@", end="")
+            print("=", end="")
         print(" ")
         waitTime += proc.duration
 
-def getInput():
+def getInput(amount):
     procArray = [] 
-    for i in range(1, 5):
+    sumOfTime = 0
+    for i in range(amount):
         print("Process ", i)
         duration = int(input("Duration: "))
         insertTime = int(input("Time of Insertion: "))
-        priority = int(input("Priority: "))
+        sumOfTime += duration
         print(" ")
-        p = Process(duration, insertTime, priority) 
+        if sumOfTime > 80:
+            print("You've reached the maximum time of duration! The last process will be killed.")
+            return procArray
+        p = Process(duration, insertTime) 
         procArray.append(p)
     return procArray
 
 def main():
-    processes = getInput()
+    amountProcesses = 11
+    print('Welcome to the FIFO algorithm!')
+    while amountProcesses > 10:
+        amountProcesses = int(input('How many processes will it be? 10 processes is the maximum: '))
+    processes = getInput(amountProcesses)
     newProcs = order(processes)
     ganttFifo(newProcs)
 
